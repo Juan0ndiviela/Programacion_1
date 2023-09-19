bfe98162567f57ebc4a5e5b0dffee292d1fc16f9
Juan0ndiviela <113891041+Juan0ndiviela@users.noreply.github.com>
2023-09-19 16:06:58 +0200

    Add files via upload

---
+++ programacion1/practica1-master/bienvenida.cpp
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * Curso de Programación 1. Tema 1
+ * Autores: Miguel Ángel Latre y Javier Martínez
+ * Modificado por: Juan Ondiviela Pamplona
+ * Última revisión: 18 de septiembre de 2021
+ * Resumen: Tareas 1 y 2
+ ******************************************************************************/
+#include <iostream>
+using namespace std;
+/*
+ * Programa que escribe en la pantalla el mensaje
+ * “Bienvenidos a UNIZAR”
+ */
+int main() {
+    std::cout << "Bienvenidos a UNIZAR" << std::endl;
+    cout << "Bienvenidos a Prog1" <<endl;
+}
---
+++ programacion1/practica1-master/eco-numerico.cpp
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Curso de Programación 1. Tema 1
+ * Autores: Miguel Ángel Latre
+ * Modificado por: Juan Ondiviela Pamplona
+ * Última revisión: 18 de septiembre de 2021
+ * Resumen: Tareas 3 y 4
+ ******************************************************************************/
+#include <iostream>
+using namespace std;
+
+/*
+ * Programa que solicita un número entero al usuario y lo escribe en pantalla.
+ */
+int main() {
+    cout << "Escriba dos numeros enteros: ";
+    int numero1;
+    int numero2;
+    cin>>numero1;
+    cin>>numero2;
+    int suma=numero1+numero2;
+    cout<<"La suma de los dos numeros introducidos es: "<<suma<<endl;
+    
+}
